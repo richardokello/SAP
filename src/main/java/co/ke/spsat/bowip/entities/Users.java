e5558293326f1273e3ea4021ada14547dc3593a6
unknown <Richard Okello>
2024-02-15 16:31:09 +0300

    Created user entity for user registration

---
+++ src/main/java/co/ke/spsat/bowip/entities/Users.java
@@ -0,0 +1,69 @@
+package co.ke.spsat.bowip.entities;
+
+import jakarta.annotation.Nonnull;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.hibernate.validator.constraints.Email;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+import java.time.Instant;
+import java.util.Date;
+@Data
+@Entity
+@Table(name = "USERS")
+@AllArgsConstructor
+public class Users {
+    @Id
+    @Column(name = "USER_ID")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "USERS_SEQ")
+    @SequenceGenerator(sequenceName = "users_seq", allocationSize = 1, name = "USERS_SEQ")
+    private Long userId;
+    @NotNull
+  //  @Pattern(regexp = Constants.LOGIN_REGEX)
+    @Size(min = 1, max = 50)
+    @Column(length = 50, unique = true, nullable = false)
+    private String login;
+
+
+
+    @Column(name = "FIRST_NAME")
+    private String firstName;
+    @Column(name = "LAST_NAME")
+    private String lastName;
+    @Column(name = "EMAIL")
+    @Nonnull
+    @Email(message = "email invalid")
+    private String email;
+    @Pattern(regexp = "(^[0-9]+$|^$)", message = "number only")
+    @Column(name = "PHONE")
+    private String phone;
+    @Column(name = "USERNAME")
+    private String username;
+    @Column(name = "PASWORD")
+    private String password;
+    @Column(name = "CREATED_AT")
+    private Date createdAt;
+    @Column(name = "CREATED_BY")
+    private String createdBy;
+    @Column(name = "DELETED_BY")
+    private String deletedBy;
+    @Column(name = "UPDATED_BY")
+    private String updatedBy;
+    private Instant modifiedAt;
+
+    @Size(max = 256)
+    @Column(name = "image_url", length = 256)
+    private String imageUrl;
+
+    @NotNull
+    @Column(nullable = false)
+    private boolean activated = false;
+
+    public Users() {
+
+    }
+}
+

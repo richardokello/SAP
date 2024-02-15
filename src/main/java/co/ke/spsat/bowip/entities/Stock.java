dd0b62b8f819588a35a50c4d1182254441fb3e67
unknown <Richard Okello>
2024-02-15 16:34:30 +0300

    created fields for the stock class

--- src/main/java/co/ke/spsat/bowip/entities/Stock.java
+++ src/main/java/co/ke/spsat/bowip/entities/Stock.java
@@ -1,4 +1,49 @@
 package co.ke.spsat.bowip.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+import org.springframework.lang.NonNull;
+
+import java.util.Date;
+
+@Data
+@Entity
+@Table(name = "STOCK")
 public class Stock {
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,generator = "PRODUCT_SEQ")
+    @SequenceGenerator(name = "PRODUCT_SEQ", sequenceName = "\"Product_Seq\"",allocationSize = 1)
+    @Id
+    @Column(name = "PRODUCT_ID")
+    @NonNull
+    private String stockId;
+    @ManyToOne(fetch = FetchType.LAZY,  cascade = CascadeType.ALL)
+    @JsonIgnore
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @Column(name = "PRODUCT_ID", nullable = false)
+    @JoinColumn(name = "PRODUCT_ID", referencedColumnName = "PRODUCT_ID", insertable = false,updatable = false)
+    private Products productId; // Reference to the corresponding product
+    private int quantity;
+    @ManyToOne(fetch = FetchType.LAZY,  cascade = CascadeType.ALL)
+    @JsonIgnore
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @Column(name = "LOCATIONID", nullable = false)
+    @JoinColumn(name = "LOCATIONID", referencedColumnName = "LOCATION_ID", insertable = false,updatable = false)
+    private Location location;
+    private int minStockLevel;
+    private int maxStockLevel;
+    private int reorderPoint;
+    private Date arrivalDate; // Use java.util.Date for simplicity
+    @ManyToOne(fetch = FetchType.LAZY,  cascade = CascadeType.ALL)
+    @JsonIgnore
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @Column(name = "BATCH_ID", nullable = false)
+    @JoinColumn(name = "BATCH_ID", referencedColumnName = "BATCH_ID", insertable = false,updatable = false)
+    private Batch batchNumber;
+
+    public Stock() {
+
+    }
 }

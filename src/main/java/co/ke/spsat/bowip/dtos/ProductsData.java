87e7259c4621ab4e71a3642799e7c27b3c99aabc
unknown <Richard Okello>
2024-02-16 12:29:44 +0300

    created record class for products

--- src/main/java/co/ke/spsat/bowip/dtos/ProductsData.java
+++ src/main/java/co/ke/spsat/bowip/dtos/ProductsData.java
@@ -1,2 +1,11 @@
-package co.ke.spsat.bowip.dtos;public record ProductsData() {
+package co.ke.spsat.bowip.dtos;
+import co.ke.spsat.bowip.entities.Batch;
+import co.ke.spsat.bowip.entities.Categories;
+import java.math.BigDecimal;
+import java.util.List;
+
+public record ProductsData(  Long productId, String productName, String brand, String description, String colour,
+                             String size, String usageInstructions, String weight, Categories categoryId, Long categoriesId,
+                             String length, String productCode,String image, BigDecimal price, Integer quantityInStock,
+                             List<Batch>batches, String SKU, String status) {
 }

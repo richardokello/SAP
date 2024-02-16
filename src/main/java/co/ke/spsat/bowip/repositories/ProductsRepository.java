a02e0d1e957c116e48a23ef70d49e68ca355be71
unknown <Richard Okello>
2024-02-16 12:16:35 +0300

    created a repositoy for products

--- src/main/java/co/ke/spsat/bowip/repositories/ProductsRepository.java
+++ src/main/java/co/ke/spsat/bowip/repositories/ProductsRepository.java
@@ -1,2 +1,7 @@
-package co.ke.spsat.bowip.repositories;public interface ProductsRepository {
+package co.ke.spsat.bowip.repositories;
+
+import co.ke.spsat.bowip.entities.Products;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductsRepository extends JpaRepository<Products, Long> {
 }

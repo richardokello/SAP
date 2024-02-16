cf229533b87bdab21e11766415d202c7614a9216
unknown <Richard Okello>
2024-02-16 12:23:16 +0300

    created response class for product service

--- src/main/java/co/ke/spsat/bowip/dtos/ProductResponse.java
+++ src/main/java/co/ke/spsat/bowip/dtos/ProductResponse.java
@@ -1,2 +1,10 @@
-package co.ke.spsat.bowip.dtos;public class ProductResponse {
+package co.ke.spsat.bowip.dtos;
+
+import lombok.Data;
+
+@Data
+public class ProductResponse {
+    private String message;
+    private String responseCode;
+
 }

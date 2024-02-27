502e616ba029dda2a649791e70a9be59a9c790f8
unknown <Richard Okello>
2024-02-27 11:52:03 +0300

    created service class for customer

--- src/main/java/co/ke/spsat/bowip/service/CustomerService.java
+++ src/main/java/co/ke/spsat/bowip/service/CustomerService.java
@@ -1,2 +1,13 @@
-package co.ke.spsat.bowip.service;public class CustomerService {
+package co.ke.spsat.bowip.service;
+
+import co.ke.spsat.bowip.dtos.CustomerDataRequest;
+import co.ke.spsat.bowip.dtos.CustomerResponseData;
+import co.ke.spsat.bowip.entities.Customers;
+
+public class CustomerService {
+    public CustomerResponseData createCustomers(CustomerDataRequest customerRequest){
+        Customers customers=new Customers();
+        return null;
+    }
+
 }

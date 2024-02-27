303df90aafef9151f9ff31fb5243dfa8e88440c9
unknown <Richard Okello>
2024-02-27 11:58:37 +0300

    craeted repository for batch that helps for saving batches of the products to batch table

--- src/main/java/co/ke/spsat/bowip/repositories/BatchRepository.java
+++ src/main/java/co/ke/spsat/bowip/repositories/BatchRepository.java
@@ -1,2 +1,9 @@
-package co.ke.spsat.bowip.repositories;public interface BatchRepository {
+package co.ke.spsat.bowip.repositories;
+
+import co.ke.spsat.bowip.entities.Batch;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BatchRepository extends JpaRepository<Batch,Long> {
 }

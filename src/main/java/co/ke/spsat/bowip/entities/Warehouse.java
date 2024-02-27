5ae62389bc28cbdba0ecfbcb56e5436e583b2e36
unknown <Richard Okello>
2024-02-27 11:59:20 +0300

    created the warehouse entity

--- src/main/java/co/ke/spsat/bowip/entities/Warehouse.java
+++ src/main/java/co/ke/spsat/bowip/entities/Warehouse.java
@@ -1,19 +1,32 @@
 package co.ke.spsat.bowip.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.annotation.Nonnull;
+import jakarta.persistence.*;
 import lombok.Data;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 @Data
 @Entity
 @Table(name = "WAREHOUSE")
 public class Warehouse {
     @Id
+    @Column(name = "WAREHOUSE_ID")
     private Long warehouseID;
+
+    @Nonnull
+    @Column(name = "WAREHOUSE_NAME")
     private String warehouseName;
-    @ManyToOne
+
+    @JsonIgnore
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JoinColumn(name = "LOCATION", referencedColumnName = "LOCATION_ID")
+    @ManyToOne(fetch = FetchType.LAZY,  cascade = CascadeType.ALL, optional = false)
     private Location location;
+    @ManyToOne(fetch = FetchType.LAZY,  cascade = CascadeType.ALL, optional = false)
+    @JsonIgnore
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JoinColumn(name = "REGIONS", referencedColumnName = "REGION_ID")
     private Regions regions;
 }

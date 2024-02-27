43d4099b2234d7d7ed8e621f67fc01b26f550493
unknown <Richard Okello>
2024-02-27 11:57:22 +0300

    created the Customer repository for saving customer details

--- src/main/java/co/ke/spsat/bowip/repositories/CustomerRepository.java
+++ src/main/java/co/ke/spsat/bowip/repositories/CustomerRepository.java
@@ -1,2 +1,7 @@
-package co.ke.spsat.bowip.repositories;public interface CustomerRepository {
+package co.ke.spsat.bowip.repositories;
+
+import co.ke.spsat.bowip.entities.Customers;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<Customers, Long> {
 }

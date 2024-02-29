72b912a056498502ebdaddafdb81ad814723ec83
unknown <Richard Okello>
2024-02-29 16:53:59 +0300

    created class object for carrying customer data

--- src/main/java/co/ke/spsat/bowip/dtos/CustomerDataRequest.java
+++ src/main/java/co/ke/spsat/bowip/dtos/CustomerDataRequest.java
@@ -2,6 +2,7 @@ package co.ke.spsat.bowip.dtos;
 
 import co.ke.spsat.bowip.entities.Address;
 import co.ke.spsat.bowip.entities.Order;
+import co.ke.spsat.bowip.entities.Regions;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.annotation.Nonnull;
 import jakarta.persistence.*;
@@ -11,23 +12,28 @@ import lombok.NoArgsConstructor;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
 
+
+import javax.validation.constraints.Pattern;
 import java.util.List;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class CustomerDataRequest {
-
     private Long customerId;
-
     private String firstName;
-
     private String lastName;
     @Column(name = "EMAIL", nullable = false)
+     private Address address;
+     private Regions regions;
+    @Pattern(regexp = "(^[0-9]+$|^$)", message = "number only")
+    private String phone;
     private String email;
 
     private Address shippingAddress;
-
-    private Address billingAddress;
-
-    private List<Order> orders;
+    private String businessName;
+    private String localRegistrationNumber;
+    private String businessLicenseNumber;
+    private String KRAPIN;
+    private String businessPrimaryContactNo;
+    private String businessSecondaryContactNo;
 }

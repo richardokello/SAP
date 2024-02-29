b8326914b3c40dd17e3061a05804203827db8a67
unknown <Richard Okello>
2024-02-29 16:51:02 +0300

    address repository

--- src/main/java/co/ke/spsat/bowip/repositories/AddressRepository.java
+++ src/main/java/co/ke/spsat/bowip/repositories/AddressRepository.java
@@ -1,2 +1,7 @@
-package co.ke.spsat.bowip.repositories;public interface AddressRepository {
+package co.ke.spsat.bowip.repositories;
+
+import co.ke.spsat.bowip.entities.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AddressRepository extends JpaRepository<Address, Long> {
 }

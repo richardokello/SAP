97a1f40be28ca31b5334b31da52010887186db24
unknown <Richard Okello>
2024-03-12 22:44:49 +0300

    created roles class

--- src/main/java/co/ke/spsat/bowip/entities/Roles.java
+++ src/main/java/co/ke/spsat/bowip/entities/Roles.java
@@ -1,2 +1,45 @@
-package co.ke.spsat.bowip.entities;public class Roles {
+package co.ke.spsat.bowip.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.Date;
+import java.util.Set;
+@Data
+@Table(name = "ROLES")
+@Entity
+public class Roles {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "ROLE_ID")
+    private Long roleId;
+    @Basic(optional = false)
+@NotNull
+@Size(min = 1, max = 40)
+@Column(name = "ROLE_NAME")
+private String roleName;
+    @Size(max = 100)
+    @Column(name = "DESCRIPTION")
+    private String description;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "CREATION_DATE")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date creationDate;
+    @Size(max = 15)
+    @Column(name = "ACTION")
+    private String action;
+    @Size(max = 15)
+    @Column(name = "ACTION_STATUS")
+    private String actionStatus;
+    @Size(max = 3)
+    @Column(name = "INTRASH")
+    private String intrash;
+
+    @Transient
+    private Set<Short> permissions;
 }

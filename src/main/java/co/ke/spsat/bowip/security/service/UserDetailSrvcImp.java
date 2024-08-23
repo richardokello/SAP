35e0bab7af80b9f9f5b8783a61fb0cb61baa8280
unknown <Richard Okello>
2024-08-23 20:31:57 +0300

    added security service for user authentication and Authorisation

--- src/main/java/co/ke/spsat/bowip/security/service/UserDetailSrvcImp.java
+++ src/main/java/co/ke/spsat/bowip/security/service/UserDetailSrvcImp.java
@@ -1,2 +1,27 @@
-package co.ke.spsat.bowip.security.service;public class UserDetailSrvcImp {
+package co.ke.spsat.bowip.security.service;
+
+import co.ke.spsat.bowip.repositories.UsersRepository;
+import co.ke.spsat.bowip.user.Users;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class UserDetailSrvcImp implements UserDetailsService {
+    private final UsersRepository repository;
+
+
+    public UserDetailSrvcImp(UsersRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return repository.findByUsername(username)
+                .orElseThrow(()-> new UsernameNotFoundException("User not found"));
+    }
+
 }

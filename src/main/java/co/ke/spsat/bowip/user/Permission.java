774fcb445e6d5ea82271cf283ff4ad6cea87d19f
unknown <Richard Okello>
2024-08-23 22:41:18 +0300

    created class to carry permission assigned to user for authorization

--- src/main/java/co/ke/spsat/bowip/user/Permission.java
+++ src/main/java/co/ke/spsat/bowip/user/Permission.java
@@ -1,2 +1,19 @@
-package co.ke.spsat.bowip.user;public enum Permission {
+package co.ke.spsat.bowip.user;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public enum Permission {
+    ADMIN_READ("admin:read"),
+    ADMIN_UPDATE("admin:update"),
+    ADMIN_CREATE("admin:create"),
+    ADMIN_DELETE("admin:delete"),
+    MEMBER_READ("management:read"),
+    MEMBER_UPDATE("management:update"),
+    MEMBER_CREATE("management:create"),
+    MEMBER_DELETE("management:delete")
+    ;
+    @Getter
+    private final String permission;
 }

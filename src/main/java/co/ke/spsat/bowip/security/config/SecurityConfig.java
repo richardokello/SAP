090a12d03ba6dc731b51d5195dcebe286919f0c4
unknown <Richard Okello>
2024-08-23 23:21:25 +0300

    class that congifures security bean

--- src/main/java/co/ke/spsat/bowip/security/config/SecurityConfig.java
+++ src/main/java/co/ke/spsat/bowip/security/config/SecurityConfig.java
@@ -1,2 +1,61 @@
-package co.ke.spsat.bowip.security.config;public class SecurityConfig {
+package co.ke.spsat.bowip.security.config;
+
+import co.ke.spsat.bowip.security.filter.JWTAuthFilter;
+import co.ke.spsat.bowip.security.service.UserDetailSrvcImp;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    private  final UserDetailSrvcImp userDetailSrvcImp;
+    private final JWTAuthFilter jwtAuthenticationFilter;
+
+    private final CustomLogoutHandler logoutHandler;
+
+    public SecurityConfig(UserDetailSrvcImp userDetailSrvcImp, JWTAuthFilter jwtAuthenticationFilter, CustomLogoutHandler logoutHandler) {
+        this.userDetailSrvcImp = userDetailSrvcImp;
+        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+        this.logoutHandler = logoutHandler;
+    }
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(
+                        req->req.requestMatchers("/auth/**")
+                                .permitAll()
+                                //.requestMatchers("/admin_only/**").hasAuthority("ADMIN")
+                                .anyRequest()
+                                .authenticated()
+                ).userDetailsService(userDetailSrvcImp)
+                .sessionManagement(session->session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .exceptionHandling(
+                        e->e.accessDeniedHandler(
+                                        (request, response, accessDeniedException)->response.setStatus(403)
+                                )
+                                .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)))
+                .logout(l->l
+                        .logoutUrl("/auth/logout")
+                        .addLogoutHandler(logoutHandler)
+                        .logoutSuccessHandler((request, response, authentication) -> SecurityContextHolder.clearContext()
+                        ))
+                .build();
+//"/api/v1/login/**","/api/v1/register/**", "/api/v1/refresh_token/**"
+    }
+
 }

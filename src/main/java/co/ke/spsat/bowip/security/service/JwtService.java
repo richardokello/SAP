7d6cadc4a03a5ae32fb6338bda6f3155edac8916
unknown <Richard Okello>
2024-08-23 22:44:57 +0300

    service for handing generating jwt token access token and refresh

--- src/main/java/co/ke/spsat/bowip/security/service/JwtService.java
+++ src/main/java/co/ke/spsat/bowip/security/service/JwtService.java
@@ -1,2 +1,95 @@
-package co.ke.spsat.bowip.security.service;public class JwtService {
+package co.ke.spsat.bowip.security.service;
+
+import co.ke.spsat.bowip.repositories.TokenRepository;
+import co.ke.spsat.bowip.user.Users;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+import java.util.function.Function;
+
+@Service
+public class JwtService {
+    @Value("${application.security.jwt.secret-key}")
+    private String secretKey;
+
+    @Value("${application.security.jwt.access-token-expiration}")
+    private long accessTokenExpire;
+
+    @Value("${application.security.jwt.refresh-token-expiration}")
+    private long refreshTokenExpire;
+    private final TokenRepository tokenRepository;
+
+    public JwtService(TokenRepository tokenRepository) {
+        this.tokenRepository = tokenRepository;
+    }
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+    public boolean isValid(String token, UserDetails user) {
+        String username = extractUsername(token);
+        boolean validToken = tokenRepository
+                .findByAccessToken(token)
+                .map(t -> !t.isLoggedOut())
+                .orElse(false);
+        return (username.equals(user.getUsername())) && !isTokenExpired(token) && validToken;
+    }
+    public boolean isValidRefreshToken(String token, Users user) {
+        String username = extractUsername(token);
+
+        boolean validRefreshToken = tokenRepository
+                .findByRefreshToken(token)
+                .map(t -> !t.isLoggedOut())
+                .orElse(false);
+
+        return (username.equals(user.getUsername())) && !isTokenExpired(token) && validRefreshToken;
+    }
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    public <R> R extractClaim(String token, Function<Claims,R> claimParser){
+        final  Claims claims=extractAllClaims(token);
+        return claimParser.apply(claims);
+    }
+    private Claims extractAllClaims(String token) {
+        return Jwts
+                .parser()
+                .verifyWith(getSigninKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+    public String generateAccessToken(Users user) {
+        return generateToken(user, accessTokenExpire);
+    }
+
+    public String generateRefreshToken(Users user) {
+        return generateToken(user, refreshTokenExpire );
+    }
+
+    private String generateToken(Users user, long expireTime) {
+        String token = Jwts
+                .builder()
+                .subject(user.getUsername())
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + expireTime ))
+                .signWith(getSigninKey())
+                .compact();
+
+        return token;
+    }
+    private SecretKey getSigninKey() {
+        byte[] keyBytes = Decoders.BASE64URL.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
 }

a2d9c3e39c8f78914a4378c86faae9ba6b82f827
unknown <Richard Okello>
2024-08-23 23:14:44 +0300

    class that manages user logout fucntion

--- src/main/java/co/ke/spsat/bowip/security/config/CustomLogoutHandler.java
+++ src/main/java/co/ke/spsat/bowip/security/config/CustomLogoutHandler.java
@@ -1,2 +1,38 @@
-package co.ke.spsat.bowip.security.config;public class CustomLogoutHandler {
+package co.ke.spsat.bowip.security.config;
+
+import co.ke.spsat.bowip.repositories.TokenRepository;
+import co.ke.spsat.bowip.security.Token;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+
+@Configuration
+public class CustomLogoutHandler  implements LogoutHandler {
+
+    private final TokenRepository tokenRepository;
+
+    public CustomLogoutHandler(TokenRepository tokenRepository) {
+        this.tokenRepository = tokenRepository;
+    }
+
+    @Override
+    public void logout(HttpServletRequest request,
+                       HttpServletResponse response,
+                       Authentication authentication) {
+        String authHeader = request.getHeader("Authorization");
+
+        if(authHeader == null || !authHeader.startsWith("Bearer ")) {
+            return;
+        }
+
+        String token = authHeader.substring(7);
+        Token storedToken = tokenRepository.findByAccessToken(token).orElse(null);
+
+        if(storedToken != null) {
+            storedToken.setLoggedOut(true);
+            tokenRepository.save(storedToken);
+        }
+    }
 }

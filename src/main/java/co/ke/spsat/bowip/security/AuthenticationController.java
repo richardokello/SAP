f3fc529a8b1050800c132712e5db968dd307dfd6
unknown <Richard Okello>
2024-08-23 23:10:34 +0300

    controller  class for user login and registration

--- src/main/java/co/ke/spsat/bowip/security/AuthenticationController.java
+++ src/main/java/co/ke/spsat/bowip/security/AuthenticationController.java
@@ -1,2 +1,45 @@
-package co.ke.spsat.bowip.security;public class AuthenticationController {
+package co.ke.spsat.bowip.security;
+
+import co.ke.spsat.bowip.security.service.AuthService;
+import co.ke.spsat.bowip.user.Users;
+import co.ke.spsat.bowip.user.dto.SignInDTO;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class AuthenticationController {
+    private final AuthService authService;
+
+    public AuthenticationController(AuthService authService) {
+        this.authService = authService;
+    }
+
+
+    @PostMapping("/auth/register")
+    public ResponseEntity<AuthenticationResponse> register(
+            @RequestBody Users request,  Long departmentId
+    ) {
+        return ResponseEntity.ok(authService.register(request ,departmentId));
+    }
+
+    @PostMapping("/auth/login")
+    public ResponseEntity<AuthenticationResponse> login(
+            @RequestBody Users request
+    ) {
+        return ResponseEntity.ok(authService.authenticate(request));
+     }
+
+    @PostMapping("/auth/refresh_token")
+    public ResponseEntity refreshToken(
+            HttpServletRequest request,
+            HttpServletResponse response
+    ) {
+        return authService.refreshToken(request, response);
+    }
 }

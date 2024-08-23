d8795d2751cd1f2a2b78307c6431e890146eb91b
unknown <Richard Okello>
2024-08-23 23:12:46 +0300

    entity class that stores token

--- src/main/java/co/ke/spsat/bowip/security/Token.java
+++ src/main/java/co/ke/spsat/bowip/security/Token.java
@@ -1,2 +1,35 @@
-package co.ke.spsat.bowip.security;public class Token {
+package co.ke.spsat.bowip.security;
+
+import co.ke.spsat.bowip.user.Users;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Entity
+
+public class Token {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "access_token")
+    private String accessToken;
+
+    @Column(name = "refresh_token")
+    private String refreshToken;
+
+    @Column(name = "is_logged_out")
+    private boolean loggedOut;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private Users user;
+
 }

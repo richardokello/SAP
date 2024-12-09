49f421ada185be6b95af90182d3ed254712f8770
unknown <Richard Okello>
2024-12-09 12:10:25 +0300

    create the fucntion to check the expiration dates

---
+++ src/main/java/co/ke/spsat/bowip/controller/BatchAutomationController.java
@@ -0,0 +1,26 @@
+package co.ke.spsat.bowip.controller;
+
+import co.ke.spsat.bowip.service.BatchAutomationService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/batches/automation")
+public class BatchAutomationController {
+    private final BatchAutomationService batchAutomationService;
+    // Constructor for dependency injection
+    public BatchAutomationController(BatchAutomationService batchAutomationService) {
+        this.batchAutomationService = batchAutomationService;
+    }
+
+
+    // Endpoint to trigger quality and expiration check
+    @GetMapping("/quality-control")
+    public ResponseEntity<String> triggerQualityControlCheck() {
+        batchAutomationService.automateQualityControl();
+        return ResponseEntity.ok("Quality and expiration check triggered.");
+    }
+
+}
